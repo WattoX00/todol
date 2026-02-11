bdc63c1b4a7c3d7523c402facd81b5de13a0402f
Wattox <78638839+WattoX00@users.noreply.github.com>
2026-02-11 08:28:07 +0100

    Refactor ShellCompleter for improved command completion
    Refactor ShellCompleter to improve completion logic and handle tags.

--- todol/functionality/shellcompleter.py
+++ todol/functionality/shellcompleter.py
@@ -1,10 +1,15 @@
 from prompt_toolkit.completion import Completer, Completion
 
+from prompt_toolkit.completion import Completer, Completion
+
 class ShellCompleter(Completer):
     def get_completions(self, document, complete_event):
-        
+
         from .commands_list import COMMANDS
-        
+        from .functions import Functions
+        # tags.py
+        TAGS = Functions.load_tags()
+
         if not complete_event.completion_requested:
             return
 
@@ -16,19 +21,19 @@ class ShellCompleter(Completer):
                 yield Completion(cmd, start_position=0)
             return
 
+        last = words[-1]
+
+        if last.startswith("@"):
+            current = last
+            for tag in TAGS:
+                if tag.startswith(current):
+                    yield Completion(tag, start_position=-len(current))
+            return
+
         if len(words) == 1 and not text.endswith(" "):
             current = words[0]
             for cmd in COMMANDS:
                 if cmd.startswith(current):
                     yield Completion(cmd, start_position=-len(current))
             return
-
-        cmd = words[0]
-        args = COMMANDS.get(cmd, [])
-
-        if args:
-            current = words[-1] if not text.endswith(" ") else ""
-            for arg in args:
-                if arg.startswith(current):
-                    yield Completion(arg, start_position=-len(current))
-
+        return
