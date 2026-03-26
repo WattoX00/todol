a93b1aefc404a4e8ad71519894a3daf74fcffd22
wattox00 <korschzozinho@gmail.com>
2026-03-26 18:30:44 +0100

    Append bug fix

--- todol/functionality/shellcompleter.py
+++ todol/functionality/shellcompleter.py
@@ -1,13 +1,11 @@
 from prompt_toolkit.completion import Completer, Completion
 
-from prompt_toolkit.completion import Completer, Completion
-
 class ShellCompleter(Completer):
     def get_completions(self, document, complete_event):
 
         from .commands_list import COMMANDS
         from .functions import Functions
-        # tags.py
+
         TAGS = Functions.load_tags()
 
         if not complete_event.completion_requested:
@@ -18,7 +16,7 @@ class ShellCompleter(Completer):
 
         if not words:
             for cmd in COMMANDS:
-                yield Completion(cmd, start_position=0)
+                yield Completion(cmd + " ", start_position=0)
             return
 
         last = words[-1]
@@ -27,13 +25,14 @@ class ShellCompleter(Completer):
             current = last
             for tag in TAGS:
                 if tag.startswith(current):
-                    yield Completion(tag, start_position=-len(current))
+                    yield Completion(tag + " ", start_position=-len(current))
             return
 
         if len(words) == 1 and not text.endswith(" "):
             current = words[0]
             for cmd in COMMANDS:
                 if cmd.startswith(current):
-                    yield Completion(cmd, start_position=-len(current))
+                    yield Completion(cmd + " ", start_position=-len(current))
             return
+
         return
